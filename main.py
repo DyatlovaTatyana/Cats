2f7e8621549a2e521fa52de44afd9b6b98d31199
DyatlovaTatyana <tanya.miheleva@mail.ru>
2024-08-09 15:26:12 +0300

    Add files via upload
    Исправили переменные с ответом

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ print(response.text)
 endpoint = '/fact'
 query_params = {'max_length':30}
 
-response = requests.get(base_url+endpoint, params=query_params)
-print(response.status_code)
-print(response.text)
+response1 = requests.get(base_url+endpoint, params=query_params)
+print(response1.status_code)
+print(response1.text)
 
 
 endpoint = '/facts'
 query_params = {'max_length': 60, 'limit': 5}
 
-response = requests.get(base_url+endpoint, params=query_params)
-print(response.status_code)
-print(response.text)
+response2 = requests.get(base_url+endpoint, params=query_params)
+print(response2.status_code)
+print(response2.text)
